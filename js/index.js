1b58b019c6c980c55ecdc310331090f54110f1e4
Logan Kimball <logankimbs@gmail.com>
2021-02-22 13:20:41 -0700

    Fetched Random User Generator API

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+document.getElementById("generateOneFriend").addEventListener("click", function(event) {
+    const url = "https://randomuser.me/api/?nat=us";
+
+    fetch(url).then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        displayData(json);
+    });
+});
+
+function displayData(json) {
+    console.log(json);
+}
