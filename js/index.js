a17a33ab1f7ea05b2aacf37dc84c37115ce542a7
Logan Kimball <logankimbs@gmail.com>
2021-02-25 14:51:38 -0700

    Added card and button

--- js/index.js
+++ js/index.js
@@ -10,4 +10,22 @@ document.getElementById("generateOneFriend").addEventListener("click", function(
 
 function displayData(json) {
     console.log(json);
+
+    let html = (`
+        <div class="card">
+            <div class="banner">
+                <div class="avatar" style="background-image: url(${json.results[0].picture.large})"></div>
+            </div>
+            <h3>${json.results[0].name.first + " " + json.results[0].name.last}</h3>
+            <a>${json.results[0].phone}</a>
+            <a>${json.results[0].email}</a>
+            <ul>
+                <a href=""><i class="fa fa-github" style="font-size:16px"></i></a>
+                <a href=""><i class="fa fa-codepen" style="font-size:16px"></i></a>
+                <a href=""><i class="fa fa-spotify" style="font-size:16px"></i></a>
+            </ul>
+        </div>
+    `);
+
+    document.getElementById("friend").innerHTML = html;
 }
